a6ef5344824dd86cc776762c667452ffd9062e35
stevenlee990 <stevenlee990@gmail.com>
2024-09-21 14:21:02 +0800

    add main2.py

--- pico/lesson4/main1.py
+++ pico/lesson4/main1.py
@@ -1,3 +1,13 @@
 from machine import Pin
+import time
+
 led = Pin("LED", Pin.OUT)
-led.value(0)
+status = False
+while True:
+    if status == False:
+        led.on()
+        status = True
+    else:
+        led.off()
+        status = False
+    time.sleep(1)
---
+++ pico/lesson4/main2.py
@@ -0,0 +1,12 @@
+from machine import Timer
+
+#tim = Timer(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
+count = 0
+def mycallback(t:Timer):
+    global count
+    count += 1
+    print(f"目前mycallback被執行:{count}次")
+    if count >= 10:
+        t.deinit()
+
+led_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=mycallback)
