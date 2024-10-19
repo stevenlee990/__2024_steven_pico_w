4a34c8c748e1969b394473397dfa63e3be1c13e1
stevenlee990 <stevenlee990@gmail.com>
2024-10-19 16:10:48 +0800

    update lession15

--- lession15/main.py
+++ lession15/main.py
@@ -21,12 +21,14 @@ def do_thing(t):
     '''
     conversion_factor = 3.3 / (65535)
     reading = adc.read_u16() * conversion_factor
-    temperature = 27 - (reading - 0.706)/0.001721  
+    temperature = round(27 - (reading - 0.706)/0.001721,2)  
     print(f'溫度:{round(temperature,2)}')
     mqtt.publish('SA-23/TEMPERATURE', f'{temperature}')
     adc_value = adc_light.read_u16()
     print(f'光線:{adc_value}')
-    mqtt.publish('SA-23/LINE_LEVEL', f'{adc_value}')
+    line_state = 0 if adc_value < 3500 else 1
+    print(f'光線:{line_state}')
+    mqtt.publish('SA-23/LINE_LEVEL', f'{line_state}')
     
     
 def do_thing1(t):
